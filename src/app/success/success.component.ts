eb3a45da31207f092335d1942d28959004973da8
Akarsh Gangwar <akarsh.gangwar@stackpro.in>
2022-07-18 13:08:31 +0530

    styled the both component

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-success',
-    templateUrl:'./success.component.html'
+    templateUrl:'./success.component.html',
+    styleUrls:["./success.component.css"],
 
 })
 export class SuccessComponent
