129d634609498c579cfd6b890613d983192f0c61
Akarsh Gangwar <akarsh.gangwar@stackpro.in>
2022-07-18 13:00:43 +0530

    outputed both warning and success component in App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuccessComponent } from './success/success.component';
 import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningComponent
+    WarningComponent, 
+    SuccessComponent,
   ],
   imports: [
     BrowserModule
--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:"success",
+    selector:'app-success',
     templateUrl:'./success.component.html'
 
 })
